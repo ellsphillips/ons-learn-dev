3f06adddf89588988a0d4ff0a411b2ff1282770b
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-04-14 16:37:38 +0100

    Example component for passing theming options

---
+++ styles/theme/Example.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  /* ... */
+  background-color: ${props => props.theme.bg.primary};
+  color: ${props => props.theme.text.primary};
+`
