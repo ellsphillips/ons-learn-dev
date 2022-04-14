571cc20e9ff015265e7692c2c86a42385bc1f592
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-04-14 16:37:04 +0100

    Theme providers wrapper components

---
+++ styles/theme/Providers.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import useDarkMode from 'use-dark-mode'
+import { darkTheme, lightTheme } from '../theme/Theme'
+
+export default ({ children }) => {
+  const { value } = useDarkMode(false, { storageKey: null, onChange: null })
+  const theme = value ? darkTheme : lightTheme
+
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  const body = <ThemeProvider theme={theme}>{children}</ThemeProvider>
+
+  // prevents ssr flash for mismatched dark mode
+  if (!mounted) {
+    return <div style={{ visibility: 'hidden' }}>{body}</div>
+  }
+
+  return body
+}
