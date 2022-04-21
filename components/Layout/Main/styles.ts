bc61d9b2266160a9714a234fb28713244b8e7ff4
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-04-21 10:23:04 +0100

    Update document colour calls

--- components/Layout/Main/styles.ts
+++ components/Layout/Main/styles.ts
@@ -1,25 +1,25 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  padding: 2rem;
-  background-color: hsl(var(--clr-grey-700));
-  border-radius: 1rem;
+	padding: 2rem;
+	background-color: var(--clr-bg-secondary);
+	border-radius: 1rem;
 
-  @media (min-width: 1024px) {
-    margin-left: 32rem;
-  }
+	@media (min-width: 1024px) {
+		margin-left: 32rem;
+	}
 `
 
 export const Header = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 `
 
 export const Link = styled.div`
-  margin-top: 2rem;
-  font-size: 3rem;
-  color: #ffd866;
-  text-decoration: none;
+	margin-top: 2rem;
+	font-size: 3rem;
+	color: #ffd866;
+	text-decoration: none;
 `
