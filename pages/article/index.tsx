fed871024b29bfab19fe8ce82d7b26b4a6fbdcb3
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-04-27 21:21:05 +0100

    Move article playground to own path

---
+++ pages/article/index.tsx
@@ -0,0 +1,34 @@
+import Card from '@/components/Card'
+import Grid from '@/components/Grid'
+import Layout from '@/components/Layout'
+import articles from '@/config/articles'
+import Link from 'next/link'
+import React from 'react'
+import { CONTENT } from '../../config/'
+
+const HomePage = () => {
+	return (
+		<Layout>
+			{/*  */}
+			{CONTENT.home}
+
+			<Grid>
+				{articles.map(article => (
+					<Link href={`/article/${article.slug}`}>
+						<a style={{ textDecoration: 'none' }}>
+							<Card
+								author={article.author}
+								title={article.title}
+								subtitle={article.subtitle}
+								profile={article.profile}
+								thumbnail={article.thumbnail}
+							/>
+						</a>
+					</Link>
+				))}
+			</Grid>
+		</Layout>
+	)
+}
+
+export default HomePage
