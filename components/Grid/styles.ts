4853ee4aa823aff61d0e9c6b9e79c078ebc3cca4
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-04-27 21:04:20 +0100

    Mobile breakpoint for grid component

--- components/Grid/styles.ts
+++ components/Grid/styles.ts
@@ -5,8 +5,12 @@ export const Container = styled.div`
 	display: grid;
 	gap: 1rem;
 
-	@media (min-width: ${breakpoints.tablet}) {
+	@media (min-width: ${breakpoints.mobile}) {
 		grid-template-columns: repeat(1, 1fr);
+	}
+
+	@media (min-width: ${breakpoints.tablet}) {
+		grid-template-columns: repeat(2, 1fr);
 	}
 
 	@media (min-width: ${breakpoints.desktop}) {
