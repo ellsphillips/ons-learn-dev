36d8ee2d6b8570d17d38adcec5358e669464e5db
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-04-27 21:02:56 +0100

    Article card grid renderable

---
+++ components/Grid/styles.ts
@@ -0,0 +1,15 @@
+import { breakpoints } from '@/styles/breakpoints'
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	display: grid;
+	gap: 1rem;
+
+	@media (min-width: ${breakpoints.tablet}) {
+		grid-template-columns: repeat(1, 1fr);
+	}
+
+	@media (min-width: ${breakpoints.desktop}) {
+		grid-template-columns: repeat(3, 1fr);
+	}
+`
