0b34c2d024802a619c88150a310fbf8bea1d88ea
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-04-27 20:57:36 +0100

    Article dummy data

---
+++ config/articles/index.ts
@@ -0,0 +1,66 @@
+export default [
+	{
+		slug: 'dataclasses-are-awesome',
+		title: 'Dataclasses are awesome',
+		subtitle: 'Learn to use dataclasses to model and represent data sturctures',
+		author: 'Elliott Phillips',
+		thumbnail:
+			'https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80',
+		// '#fc4103',
+		profile:
+			'https://raw.githubusercontent.com/ellsphillips/ons.dev/master/assets/img/logo.svg',
+	},
+	{
+		slug: 'article-2',
+		title: 'Article Two',
+		subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		author: 'Elliott Phillips',
+		thumbnail:
+			'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+		// '#fca103',
+		profile: '../../assets/img/profile/ep.jpg',
+	},
+	{
+		slug: 'article-3',
+		title: 'Article Three',
+		subtitle:
+			'Fusce sed sagittis purus. Vestibulum dignissim hendrerit pulvinar.',
+		author: 'Elliott Phillips',
+		thumbnail:
+			'https://images.unsplash.com/photo-1537884944318-390069bb8665?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+		// '#f0fc03',
+		profile: '../../assets/img/profile/ep.jpg',
+	},
+	{
+		slug: 'article-4',
+		title: 'Article Four',
+		subtitle:
+			'Morbi odio lorem, fringilla venenatis iaculis at, sollicitudin nec massa.',
+		author: 'Elliott Phillips',
+		thumbnail:
+			'https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+		// '#4efc03',
+		profile: '../../assets/img/profile/ep.jpg',
+	},
+	{
+		slug: 'article-5',
+		title: 'Article Five',
+		subtitle:
+			'Proin porta velit dui, sit amet elementum tellus ultricies fermentum.',
+		author: 'Elliott Phillips',
+		thumbnail:
+			'https://images.unsplash.com/photo-1555949963-aa79dcee981c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+		// '#03fcf0',
+		profile: '../../assets/img/profile/ep.jpg',
+	},
+	{
+		slug: 'article-6',
+		title: 'Article Six',
+		subtitle: 'Donec ac ipsum a leo fermentum aliquet.',
+		author: 'Elliott Phillips',
+		thumbnail:
+			'https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+		// '#036bfc',
+		profile: '../../assets/img/profile/ep.jpg',
+	},
+]
