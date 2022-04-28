61293c4a9ba4069e6cd422b31831492343611848
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-04-28 19:39:27 +0100

    Define default svg logo styles

---
+++ components/Logo/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const LogoDefaults = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	line-height: 1;
+	cursor: 'pointer';
+
+	transition: fill 150ms ease-out;
+`
