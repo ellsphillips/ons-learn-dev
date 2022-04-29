ca4d402e5b515fc5662026d3918dc257582046b0
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-04-29 22:00:59 +0100

    Populate article hero

---
+++ components/Layout/Hero/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Container, Heading, Paragraph, Picture } from './styles'
+
+const Hero = () => {
+	return (
+		<Container>
+			<Heading>
+				Hi, I'm Elliott, a senior Data Scientist at the Office for National
+				Statistics.
+			</Heading>
+
+			<Paragraph>
+				Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+				enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
+				exercitation amet. Nisi animcupidatat excepteur officia. Reprehenderit
+				nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+				minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
+				elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
+				Lorem duis laboris cupidatat officia voluptate. Culpa proident
+				adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
+				Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
+				Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
+				et culpa duis.
+			</Paragraph>
+
+			<Picture src="https://raw.githubusercontent.com/ellsphillips/ons.dev/master/assets/img/profile/ep.jpg" />
+		</Container>
+	)
+}
+
+export default Hero
