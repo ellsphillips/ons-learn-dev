d04a949656bea6a05297228870c85a75780e4b9b
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-05-03 17:10:55 +0100

    Flex nax items for temp links

--- components/Nav/styles.ts
+++ components/Nav/styles.ts
@@ -2,17 +2,18 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
 	display: flex;
-	flex-direction: row;
-	align-items: center;
+	flex-direction: column;
+	width: 100%;
+	gap: 0.5rem;
 `
 
 export const Item = styled.div`
 	background-color: var(--clr-bg-tertiary);
 	border-radius: 4pt;
-	color: var(--clr-fg);
-	font-size: 3rem;
-	padding: 1rem;
-	margin-inline: 0.5rem;
+	color: var(--clr-fg-primary);
+	font-size: 1.4rem;
+	font-weight: 700;
+	padding: 0.5rem 1rem;
 	cursor: pointer;
 
 	transition: color ease-in-out 150ms, background-color ease-in-out 150ms;
