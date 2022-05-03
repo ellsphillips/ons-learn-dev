a8fab7c11972f237ee85a60f37ae904a27ca8cc5
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-05-03 17:05:43 +0100

    Placeholder content for config

--- config/articles/index.ts
+++ config/articles/index.ts
@@ -1,66 +1,61 @@
+import placeholder from '../placeholder'
+
 export default [
 	{
 		slug: 'dataclasses-are-awesome',
 		title: 'Dataclasses are awesome',
 		subtitle: 'Learn to use dataclasses to model and represent data sturctures',
-		author: 'Elliott Phillips',
+		author: placeholder.author,
 		thumbnail:
 			'https://images.unsplash.com/photo-1624953587687-daf255b6b80a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
-		// '#fc4103',
-		profile:
-			'https://raw.githubusercontent.com/ellsphillips/ons.dev/master/assets/img/logo.svg',
+		profile: placeholder.profile,
 	},
 	{
 		slug: 'article-2',
-		title: 'Article Two',
+		title: 'A successful branching model',
 		subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		author: 'Elliott Phillips',
+		author: placeholder.author,
 		thumbnail:
 			'https://images.unsplash.com/photo-1556075798-4825dfaaf498?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80',
-		// '#fca103',
-		profile: '../../assets/img/profile/ep.jpg',
+		profile: placeholder.profile,
 	},
 	{
 		slug: 'article-3',
 		title: 'Article Three',
 		subtitle:
 			'Fusce sed sagittis purus. Vestibulum dignissim hendrerit pulvinar.',
-		author: 'Elliott Phillips',
+		author: placeholder.author,
 		thumbnail:
 			'https://images.unsplash.com/photo-1555952494-efd681c7e3f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-		// '#f0fc03',
-		profile: '../../assets/img/profile/ep.jpg',
+		profile: placeholder.profile,
 	},
 	{
 		slug: 'article-4',
 		title: 'Article Four',
 		subtitle:
 			'Morbi odio lorem, fringilla venenatis iaculis at, sollicitudin nec massa.',
-		author: 'Elliott Phillips',
+		author: placeholder.author,
 		thumbnail:
 			'https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-		// '#4efc03',
-		profile: '../../assets/img/profile/ep.jpg',
+		profile: placeholder.profile,
 	},
 	{
 		slug: 'article-5',
 		title: 'Article Five',
 		subtitle:
 			'Proin porta velit dui, sit amet elementum tellus ultricies fermentum.',
-		author: 'Elliott Phillips',
+		author: placeholder.author,
 		thumbnail:
 			'https://images.unsplash.com/photo-1555949963-aa79dcee981c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-		// '#03fcf0',
-		profile: '../../assets/img/profile/ep.jpg',
+		profile: placeholder.profile,
 	},
 	{
 		slug: 'article-6',
 		title: 'Article Six',
 		subtitle: 'Donec ac ipsum a leo fermentum aliquet.',
-		author: 'Elliott Phillips',
+		author: placeholder.author,
 		thumbnail:
 			'https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-		// '#036bfc',
-		profile: '../../assets/img/profile/ep.jpg',
+		profile: placeholder.profile,
 	},
 ]
