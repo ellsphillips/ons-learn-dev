3dba831ab01f01fd705a3d9ea4237f0a469a9e02
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-05-04 13:57:37 +0100

    Dynamic article page routing

--- pages/article/[slug].tsx
+++ pages/article/[slug].tsx
@@ -1,11 +1,44 @@
+import Layout from '@/components/Layout'
+import Hero from '@/components/Layout/Hero'
+import articles from '@/config/articles'
 import React from 'react'
 
-interface ThingProps {
-	header: string
+export default function ArticlePage({ article }) {
+	return (
+		<Layout>
+			<Hero
+				title={article.title}
+				caption={article.caption}
+				profile={article.profile}
+			/>
+
+			<h3>{'<Content />'}</h3>
+		</Layout>
+	)
 }
 
-const Thing = (props: ThingProps) => {
-	return <h1>{props.header}</h1>
+export async function getStaticProps({ params }) {
+	const result = articles.filter(article =>
+		article.title.toLowerCase().replace(/ /g, '-').includes(params.slug)
+	)
+
+	return {
+		props: {
+			article: result[0],
+		},
+	}
 }
 
-export default Thing
+export async function getStaticPaths() {
+	return {
+		paths: articles.map(article => {
+			const slug = article.title.toLowerCase().replace(/ /g, '-')
+			return {
+				params: {
+					slug,
+				},
+			}
+		}),
+		fallback: false,
+	}
+}
